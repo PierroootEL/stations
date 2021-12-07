8ea4b8e756c1d385fec3b9bc2b500bd911d6f182
Pierre BLANCHARD <PIERREB@FOREZIENNE.PRI>
2021-12-07 11:21:26 +0100

    Mise à jour bot

--- refresh_data/main.py
+++ refresh_data/main.py
@@ -1,21 +1,29 @@
 import requests
 import zipfile
+import datetime
+import time
 import os
 
-if os.path.isfile('PrixCarburants_quotidien_20211206.xml'):
-    print("Le fichier existe déjà !")
+export = 'C:/Users/pierreb/Documents/stations/refresh_data/' # A changer en fonction du chemin d'accès
 
-    exit(0)
-else:
-    r = requests.get('https://donnees.roulez-eco.fr/opendata/jour', allow_redirects=True)
+x = datetime.datetime.now() - datetime.timedelta(days=1)
 
-    open('PrixCarburants_quotidien_20211206.zip', 'wb').write(r.content)
+if os.path.isfile('prix.xml'):
+    print("Le fichier existe déjà ! Suppression en cours")
 
-    with zipfile.ZipFile('PrixCarburants_quotidien_20211206.zip', 'r') as zip_ref:
-        zip_ref.extractall('C:/Users/pierreb/PycharmProjects/prix_essence')
+    os.remove('prix.xml')
 
-    os.remove('PrixCarburants_quotidien_20211206.zip')
-    os.rename('PrixCarburants_quotidien_20211206.xml', 'C:/Users/pierreb/PycharmProjects/prix_essence/prix.xml')
+    time.sleep(2)
 
+
+r = requests.get('https://donnees.roulez-eco.fr/opendata/jour', allow_redirects=True)
+
+open('PrixCarburants_quotidien_' + x.strftime("%Y") + x.strftime("%m") + x.strftime("%d") + '.zip', 'wb').write(r.content)
+
+with zipfile.ZipFile('PrixCarburants_quotidien_' + x.strftime("%Y") + x.strftime("%m") + x.strftime("%d") + '.zip', 'r') as zip_ref:
+        zip_ref.extractall(export)
+
+os.remove('PrixCarburants_quotidien_' + x.strftime("%Y") + x.strftime("%m") + x.strftime("%d") + '.zip')
+os.rename('PrixCarburants_quotidien_' + x.strftime("%Y") + x.strftime("%m") + x.strftime("%d") + '.xml', export + 'prix.xml')
 
 print("Fin du programme, les données ont été actualisés / récupérées")
